a0dbd1e2cec0408238a11b4afe3c85e0412b52b1
Aliakbar Hemmati <48815079+aahemm@users.noreply.github.com>
2022-06-16 09:21:43 +0430

    Update allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&(2ep-xyfmzf!%tg!sa9(eej$tm_7wa=hb)le-_coaxit%e!sj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
