c76e86cc81809f001849c2f9639268294e6f0e50
aliakbar <aliakbar@hamravesh.com>
2022-06-16 15:54:13 +0430

    Add  drf urls

--- mysite/datadays/urls.py
+++ mysite/datadays/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'person', views.PersonViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
 ]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/person', include('datadays.urls')),
+    path('api/v1/', include('datadays.urls')),
 ]
